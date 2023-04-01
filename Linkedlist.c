0e7e49af9d00c8f55235d6ef80f13443f82c2f03
Ankit Kumar <102457011+kumar306Ankit@users.noreply.github.com>
2023-04-01 15:39:55 +0530

    Create Linkedlist.c

---
+++ Linkedlist.c
@@ -0,0 +1,519 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node
+{
+ int data;
+ struct Node *next;
+}*first=NULL;
+void create(int A[],int n)
+{
+ int i;
+ struct Node *t,*last;
+ first=(struct Node *)malloc(sizeof(struct Node));
+ first->data=A[0];
+ first->next=NULL;
+ last=first;
+ 
+ for(i=1;i<n;i++)
+ {
+ t=(struct Node*)malloc(sizeof(struct Node));
+ t->data=A[i];
+ t->next=NULL;
+ last->next=t;
+ last=t;
+ }
+}
+void Display(struct Node *p)
+{
+ while(p!=NULL)
+ {
+ printf("%d ",p->data);
+ p=p->next;
+ }
+}
+void RDisplay(struct Node *p)
+{
+ if(p!=NULL)
+ {
+ RDisplay(p->next);
+ printf("%d ",p->data);
+ 
+ }
+}
+int main()
+{
+ struct Node *temp;
+ int A[]={3,5,7,10,25,8,32,2};
+ create(A,8);
+ 
+ Display(first);
+ 
+ return 0; }
+
+
+
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+### Count and Sum Linked List 
+#include <stdio.h>
+#include <stdlib.h>
+struct Node
+{
+ int data;
+ struct Node *next;
+}*first=NULL;
+void create(int A[],int n)
+{
+ int i;
+ struct Node *t,*last;
+ first=(struct Node *)malloc(sizeof(struct Node));
+ first->data=A[0];
+ first->next=NULL;
+ last=first;
+ 
+ for(i=1;i<n;i++)
+ {
+ t=(struct Node*)malloc(sizeof(struct Node));
+ t->data=A[i];
+ t->next=NULL;
+ last->next=t;
+ last=t;
+ }
+}
+int count(struct Node *p)
+{
+ int l=0;
+ while(p)
+ {
+ l++;
+ p=p->next;
+ }
+ return l;
+}
+int Rcount(struct Node *p)
+{
+ if(p!=NULL)
+ return Rcount(p->next)+1;
+ else
+ return 0;
+} 
+int sum(struct Node *p)
+{
+ int s=0;
+ 
+ while(p!=NULL)
+ {
+ s+=p->data;
+ p=p->next;
+ }
+ return s;
+}
+int Rsum(struct Node *p)
+{
+ if(p==NULL)
+ return 0;
+ else
+ return Rsum(p->next)+p->data; }
+int main()
+{
+ int A[]={3,5,7,10,25,8,32,2};
+ create(A,8);
+ 
+ printf(“Count %d\n”,count(first));
+ printf(“Sum %d\n”,sum(first);
+ 
+ 
+ 
+ return 0; }
+        
+        
+        
+        
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+ 
+        
+        
+### Max element from Linked List 
+#include <stdio.h>
+#include <stdlib.h>
+struct Node
+{
+ int data;
+ struct Node *next;
+}*first=NULL;
+void create(int A[],int n)
+{
+ int i;
+ struct Node *t,*last;
+ first=(struct Node *)malloc(sizeof(struct Node));
+ first->data=A[0];
+ first->next=NULL;
+ last=first;
+ 
+ for(i=1;i<n;i++)
+ {
+ t=(struct Node*)malloc(sizeof(struct Node));
+ t->data=A[i];
+ t->next=NULL;
+ last->next=t;
+ last=t;
+ }
+}
+int Max(struct Node *p)
+{
+ int max=INT32_MIN;
+ 
+ while(p)
+ {
+ if(p->data>max)
+ max=p->data;
+ p=p->next;
+ }
+ return max;
+ 
+}
+int RMax(struct Node *p)
+{
+ int x=0;
+ 
+ if(p==0)
+ return INT32_MIN;
+ x=RMax(p->next);
+ if(x>p->data)
+ return x;
+ else
+ return p->data; }
+int main()
+{
+ int A[]={3,5,7,10,25,8,32,2};
+ create(A,8);
+ 
+ printf(“Max %d\n”,Max(first);
+ 
+ 
+ 
+ return 0; }
+        
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+### searcH a Linked List 
+#include <stdio.h>
+#include <stdlib.h>
+struct Node
+{
+ int data;
+ struct Node *next;
+}*first=NULL;
+void create(int A[],int n)
+{
+ int i;
+ struct Node *t,*last;
+ first=(struct Node *)malloc(sizeof(struct Node));
+ first->data=A[0];
+ first->next=NULL;
+ last=first;
+ 
+ for(i=1;i<n;i++)
+ {
+ t=(struct Node*)malloc(sizeof(struct Node));
+ t->data=A[i];
+ t->next=NULL;
+ last->next=t;
+ last=t;
+ }
+}
+struct Node * LSearch(struct Node *p,int key)
+{
+ struct Node *q;
+ 
+ while(p!=NULL)
+ {
+ if(key==p->data)
+ {
+ q->next=p->next;
+ p->next=first;
+ first=p;
+ return p;
+ }
+ q=p;
+ p=p->next;
+ }
+ return NULL;
+ 
+}
+struct Node * RSearch(struct Node *p,int key)
+{
+ if(p==NULL)
+ return NULL;
+ if(key==p->data)
+ return p;
+ return RSearch(p->next,key);
+ 
+}
+int main()
+{
+ struct Node *temp;
+ int A[]={3,5,7,10,25,8,32,2};
+ create(A,8);
+ temp=Search(first,8);
+ printf(“%d”,temp->data);
+ 
+ return 0; }
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+### iNSERT LL
+#include <stdio.h>
+#include <stdlib.h>
+struct Node
+{
+ int data;
+ struct Node *next;
+}*first=NULL;
+void create(int A[],int n)
+{
+ int i;
+ struct Node *t,*last;
+ first=(struct Node *)malloc(sizeof(struct Node));
+ first->data=A[0];
+ first->next=NULL;
+ last=first;
+ 
+ for(i=1;i<n;i++)
+ {
+ t=(struct Node*)malloc(sizeof(struct Node));
+ t->data=A[i];
+ t->next=NULL;
+ last->next=t;
+ last=t;
+ }
+}
+void Display(struct Node *p)
+{
+ while(p!=NULL)
+ {
+ printf("%d ",p->data);
+ p=p->next;
+ }
+}
+void Insert(struct Node *p,int index,int x)
+{
+ struct Node *t;
+ int i;
+ 
+ if(index < 0 || index > count(p))
+ return;
+ t=(struct Node *)malloc(sizeof(struct Node));
+ t->data=x;
+ 
+ if(index == 0)
+ {
+ t->next=first;
+ first=t;
+ }
+ else
+ {
+ for(i=0;i<index-1;i++)
+ p=p->next;
+ t->next=p->next;
+ p->next=t;
+ 
+ }
+ 
+ 
+}
+int main()
+{
+ 
+ int A[]={10,20,30,40,50};
+ create(A,5);
+ 
+ 
+ Insert(first,0,5);
+ Display(first);
+ return 0; }
+        
+        
+        
+        
+        
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+        
+        
+### Inserting in a Sorted Linked List 
+#include <stdio.h>
+#include <stdlib.h>
+struct Node
+{
+ int data;
+ struct Node *next;
+}*first=NULL,*second=NULL,*third=NULL;
+void Display(struct Node *p)
+{
+ while(p!=NULL)
+ {
+ printf("%d ",p->data);
+ p=p->next;
+ }
+}
+void create(int A[],int n)
+{
+ int i;
+ struct Node *t,*last;
+ first=(struct Node *)malloc(sizeof(struct Node));
+ first->data=A[0];
+ first->next=NULL;
+ last=first;
+ 
+ for(i=1;i<n;i++)
+ {
+ t=(struct Node*)malloc(sizeof(struct Node));
+ t->data=A[i];
+ t->next=NULL;
+ last->next=t;
+ last=t;
+ }
+}
+void SortedInsert(struct Node *p,int x)
+{
+ struct Node *t,*q=NULL;
+ 
+ t=(struct Node*)malloc(sizeof(struct Node));
+ t->data=x;
+ t->next=NULL;
+ 
+ if(first==NULL)
+ first=t;
+ else
+ {
+ while(p && p->data<x)
+ {
+ q=p;
+ p=p->next;
+ }
+ if(p==first)
+ {
+ t->next=first;
+ first=t;
+ }
+ else
+ {
+ t->next=q->next;
+ q->next=t;
+ }
+ }
+ 
+}
+int main()
+{
+ 
+ int A[]={10,20,30,40,50};
+ create(A,5);
+ 
+ 
+ printf(“%d\n”,SortedInsert(first,15)); 
+ Display(first);
+ 
+ 
+ return 0; }
+  
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+### Deleting
+#include <stdio.h>
+#include <stdlib.h>
+struct Node
+{
+ int data;
+ struct Node *next;
+}*first=NULL;
+void create(int A[],int n)
+{
+ int i;
+ struct Node *t,*last;
+ first=(struct Node *)malloc(sizeof(struct Node));
+ first->data=A[0];
+ first->next=NULL;
+ last=first;
+ 
+ for(i=1;i<n;i++)
+ {
+ t=(struct Node*)malloc(sizeof(struct Node));
+ t->data=A[i];
+ t->next=NULL;
+ last->next=t;
+ last=t;
+ }
+}
+void Display(struct Node *p)
+{
+ while(p!=NULL)
+ {
+ printf("%d ",p->data);
+ p=p->next;
+ }
+}
+void RDisplay(struct Node *p)
+{
+ if(p!=NULL)
+ {
+ RDisplay(p->next);
+ printf("%d ",p->data);
+ 
+ }
+}
+int Delete(struct Node *p,int index)
+{
+ struct Node *q=NULL;
+ int x=-1,i;
+ 
+ if(index < 1 || index > count(p))
+ return -1;
+ if(index==1)
+ {
+ q=first;
+ x=first->data;
+ first=first->next;
+ free(q);
+ return x;
+ }
+ else
+ {
+ for(i=0;i<index-1;i++)
+ {
+ q=p;
+ p=p->next;
+ }
+ q->next=p->next;
+ x=p->data;
+ free(p);
+ return x;
+ 
+ }
+ 
+ 
+}
+int main()
+{
+ 
+ int A[]={10,20,30,40,50};
+ create(A,5);
+ 
+ printf(“%d\n",Delete(first),2);
+ Display(first);
+ 
+ return 0; }
+        
+        
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------
